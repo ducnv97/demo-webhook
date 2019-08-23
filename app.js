198fe86277db1e0790c3fdcd43cce84e2e6a9897
ducnv1997 <ducnv@sphinx-software,com>
2019-08-23 11:10:39 +0700

    fix port

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', './view');
 
+require('dotenv').config();
 
 app.get('/', (req, res)=>{
-    res.send("this is test webhook");
+    res.render('index')
 })
 
 app.post('/webhook', (req, res) => {
@@ -12,10 +15,9 @@ app.post('/webhook', (req, res) => {
 
 })
 
-app.get('/test',(req, res) =>{
-    res.send("this tis test route");
-})
 
-app.listen(8000, () => {
-    console.log('server run in port 8000');
+let port = process.env.PORT || 8000
+
+app.listen(port , () => {
+    console.log('server run in port ' + port);
 })
